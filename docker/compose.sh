c9e9953cd7737d9e4bf0479b6de2eb9a6e06473b
Laci <freundl0509@gmail.com>
2024-10-08 17:23:44 +0000

    compose

--- docker/compose.sh
+++ docker/compose.sh
@@ -2,31 +2,31 @@
 
 # turn this into a docker command 
 
-SEMANTIC_MAPPING_DIR=.
+SEMANTIC_MAPPING_DIR=/home/lfreund/semantic_navigation
 DATA_DIR=/mnt/data0/ros/documents
 DOCKER_CONTAINER_NAME=semantic_navigation
+DOCKER_CACHE_DIR=.
 
 
 docker build -t semantic_navigation:latest -f Dockerfile .
-docker run -dit --rm --gpus all \
-  --name ${DOCKER_CONTAINER_NAME} \
+docker run -dit --gpus all \
+  --name semantic_navigation \
   -e DISPLAY=$DISPLAY \
   -e ACCEPT_EULA=Y \
   -e PRIVACY_CONSENT=Y \
-  --network=host
-  --runtime=nvidia
-
-  -v ${SEMANTIC_MAPPING_DIR}/docker/isaac-sim/cache/kit:/isaac-sim/kit/cache:rw \
-  -v ${SEMANTIC_MAPPING_DIR}/docker/isaac-sim/cache/ov:/root/.cache/ov:rw \
-  -v ${SEMANTIC_MAPPING_DIR}/docker/isaac-sim/cache/pip:/root/.cache/pip:rw \
-  -v ${SEMANTIC_MAPPING_DIR}/docker/isaac-sim/cache/glcache:/root/.cache/nvidia/GLCache:rw \
-  -v ${SEMANTIC_MAPPING_DIR}/docker/isaac-sim/cache/computecache:/root/.nv/ComputeCache:rw \
-  -v ${SEMANTIC_MAPPING_DIR}/docker/isaac-sim/logs:/root/.nvidia-omniverse/logs:rw \
-  -v ${SEMANTIC_MAPPING_DIR}/docker/isaac-sim/data:/root/.local/share/ov/data:rw \
-  -v ${SEMANTIC_MAPPING_DIR}/docker/isaac-sim/.vscode-server:/root/.vscode-server:rw \
+  --network=host \
+  --runtime=nvidia \
+  -v ${DOCKER_CACHE_DIR}/docker/isaac-sim/cache/kit:/isaac-sim/kit/cache:rw \
+  -v ${DOCKER_CACHE_DIR}/docker/isaac-sim/cache/ov:/root/.cache/ov:rw \
+  -v ${DOCKER_CACHE_DIR}/docker/isaac-sim/cache/pip:/root/.cache/pip:rw \
+  -v ${DOCKER_CACHE_DIR}/docker/isaac-sim/cache/glcache:/root/.cache/nvidia/GLCache:rw \
+  -v ${DOCKER_CACHE_DIR}/docker/isaac-sim/cache/computecache:/root/.nv/ComputeCache:rw \
+  -v ${DOCKER_CACHE_DIR}/docker/isaac-sim/logs:/root/.nvidia-omniverse/logs:rw \
+  -v ${DOCKER_CACHE_DIR}/docker/isaac-sim/data:/root/.local/share/ov/data:rw \
+  -v ${DOCKER_CACHE_DIR}/docker/isaac-sim/.vscode-server:/root/.vscode-server:rw \
   -v ${SEMANTIC_MAPPING_DIR}:/app \
-  -v ${DATA_DIR}:/workspace:rw \
   -v /app/docker \ 
+  -v ${DATA_DIR}:/workspace:rw \
   semantic_navigation:latest
 
   #-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
