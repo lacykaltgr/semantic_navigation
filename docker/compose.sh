c2e498c2a06ddfc889dff1e454e7c157bdd13083
Freund Laci <freundl0509@gmail.com>
2024-10-09 12:57:38 +0000

    docker

--- docker/compose.sh
+++ docker/compose.sh
@@ -2,16 +2,23 @@
 
 # turn this into a docker command 
 
-SEMANTIC_MAPPING_DIR=/home/lfreund/semantic_navigation
+SEMANTIC_MAPPING_DIR=/home/lfreund/projects/semantic_navigation
+DOCKER_CURRENT_DIR=$SEMANTIC_MAPPING_DIR/docker
 DATA_DIR=/mnt/data0/ros/documents
 DOCKER_CONTAINER_NAME=semantic_navigation
-DOCKER_CACHE_DIR=.
+DOCKER_CACHE_DIR=/home/lfreund/docker_cache
+
+mkdir conda_environments
+#cp ../conceptfusion-compact/environment.yaml /conda_environments/conceptfusion-compact.yaml
+cp $SEMANTIC_MAPPING_DIR/mission_ros_ws/src/cf_tools/environment.yaml $DOCKER_CURRENT_DIR/conda_environments/cf_tools.yaml
+cp $SEMANTIC_MAPPING_DIR/mission_ros_ws/src/local_planner/environment.yaml $DOCKER_CURRENT_DIR/conda_environments/local_planner.yaml
+cp $SEMANTIC_MAPPING_DIR/mission_ros_ws/src/mission_planner/environment.yaml $DOCKER_CURRENT_DIR/conda_environments/mission_planner.yaml
 
 
 docker build -t semantic_navigation:latest -f Dockerfile .
 docker run -dit --gpus all \
-  --name semantic_navigation1 \
-  -e DISPLAY=$DISPLAY \
+  --name semantic_navigation \
+  -e DISPLAY="" \
   -e ACCEPT_EULA=Y \
   -e PRIVACY_CONSENT=Y \
   --network=host \
@@ -25,7 +32,6 @@ docker run -dit --gpus all \
   -v ${DOCKER_CACHE_DIR}/docker/isaac-sim/data:/root/.local/share/ov/data:rw \
   -v ${DOCKER_CACHE_DIR}/docker/isaac-sim/.vscode-server:/root/.vscode-server:rw \
   -v ${SEMANTIC_MAPPING_DIR}:/app \
-  -v /app/docker \ 
   -v ${DATA_DIR}:/workspace:rw \
   semantic_navigation:latest
 
--- docker/init_script.sh
+++ docker/init_script.sh
@@ -1,22 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script is used to install the necessary dependencies for the semantic_navigation package
 
-SEMANTIC_MAPPING_DIR = /app
+SEMANTIC_MAPPING_DIR=/app
 
 
 # Install skeleton mapping
-mkdir $SEMANTIC_MAPPING_DIR/skeleton_mapping/build
-cd $SEMANTIC_MAPPING_DIR/skeleton_mapping/build
-cmake .. -DCMAKE_BUILD_TYPE=Debug && make install
+echo "Building Skeleton Finder"
+mkdir $SEMANTIC_MAPPING_DIR/skeleton-mapping/build
+cd $SEMANTIC_MAPPING_DIR/skeleton-mapping/build
+cmake .. -DCMAKE_BUILD_TYPE=Debug && make
 # TODO: add skeleton mapping to path
 
-# Install concept fusion compact
+# Build isaac-ros ws
+echo "Building Isaac ROS Humble Workspace"
+source /opt/ros/humble/setup.bash && \
+        cd /humble_ws && \
+        colcon build
+source /humble_ws/install/local_setup.bash #&& \
+    #echo "source /humble_ws/install/local_setup.bash" >> ${HOME}/.bashrc
 
-# env_manager uses conda env from hworldmodel
-#conda env create -f ./env_manager/environment.yml
 
 # Build ROS workspace
-colcon build --symlink-install
-source install/setup.bash
+echo "Building DEMO ROS Workspace"
+source /opt/ros/humble/setup.bash && \
+     cd "$SEMANTIC_MAPPING_DIR/mission_ros_ws" && \
+     colcon build
+source "$SEMANTIC_MAPPING_DIR/install/setup.bash"
 
